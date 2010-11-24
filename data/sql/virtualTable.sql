928f32464a9166bb5d9cc136b48388f2ca41489a
Luciano Stegun <lucianostegun@gmail.com>
2010-11-24 03:59:21 +0000

    Ajustes no

--- data/sql/virtualTable.sql
+++ data/sql/virtualTable.sql
@@ -1,7 +1,7 @@
 DELETE FROM virtual_table WHERE virtual_table_name = 'peopleType';
 INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'peopleType', 'Usuário administrativo', 'userAdmin', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'peopleType', 'Usuário do site', 'userSite', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'peopleType', 'Membro de ranking', 'rankingMember', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'peopleType', 'Membro de ranking', 'rankingPlayer', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 DELETE FROM virtual_table WHERE virtual_table_name = 'gameType';
 INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'gameType', 'Texas Hold''em', 'holdem', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
@@ -16,8 +16,9 @@ INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'gameStyle', 'Sit
 
 DELETE FROM virtual_table WHERE virtual_table_name = 'rankingType';
 INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'rankingType', 'Valor', 'value', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'rankingType', 'Posição', 'position', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'rankingType', 'Posição', 'score', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'rankingType', 'Balanço', 'balance', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'rankingType', 'Média x Eventos', 'average', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 DELETE FROM virtual_table WHERE virtual_table_name = 'userSiteOption';
 INSERT INTO virtual_table VALUES(nextval('virtual_table_seq'), 'userSiteOption', 'Receber confirmação de presença dos convidados para os eventos', 'receiveFriendEventConfirmNotify', true, true, false, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
