f3f2bd714f4a74b21022034cfdbf58b0c11fc896
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2011-01-20 20:09:09 +0000

    Modificações no layout da home Inclusão de informações adicionais

--- trunk/apps/frontend/modules/home/templates/_component/welcome.php
+++ trunk/apps/frontend/modules/home/templates/_component/welcome.php
@@ -1,16 +1,15 @@
 <div class="commonBar"><span>Home</span></div>
     
     <table border="0" cellspacing="0" cellpadding="0" class="welcome">
-
       <tr>
-        <td width="245" align="left" valign="top" bgcolor="#173211">
+        <td width="245" align="left" bgcolor="#173211">
         	<img src="/images/frontend/layout/welcome_img.jpg" alt="" width="245" height="162" /></td>
-        <td align="left" valign="top" style="padding:8px 10px 0px 21px; color: #DADADA" bgcolor="#173211">
+        <td align="left" valign="top" style="padding:0px 10px 0px 21px; color: #DADADA" bgcolor="#173211">
         	<p><img src="/images/frontend/layout/welcome_heading.gif" alt="" />
         		<img src="/images/frontend/layout/welcomeLogo.png" align="right"/></p>
         	
           <p>O iRank é um site gratuito para criação e organização de rankings online para jogos de Poker.</p>
 		  <p>Aqui você poderá criar seus próprios rankings, definir as configurações de jogo e organizar os eventos, classificações e estatísticas dos torneios entre seus amigos.</p>
-		  <p>Se você ainda não é cadastrado, <?php echo link_to('clique aqui', 'sign', array('class'=>'white', 'style'=>'font-weight: bold')) ?> e comece agora mesmo a controlar seus jogos!</p></td>
+		  <p>Se você ainda não é cadastrado, <?php echo link_to('clique aqui', 'sign', array('class'=>'white', 'style'=>'font-weight: bold; color: #FFFFFF')) ?> e comece agora mesmo a controlar seus jogos!</p></td>
       </tr>
     </table>
