759dbe967332cd8aa759f61eefe00f000983a571
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2011-01-21 16:19:38 +0000

    Correção da permissão para acesso ao módulo myAccount

--- trunk/data/symfony/web/errors/error500.php
+++ trunk/data/symfony/web/errors/error500.php
@@ -8,10 +8,21 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="title" content="symfony project" />
 <meta name="robots" content="index, follow" />
-<meta name="description" content="symfony project" />
-<meta name="keywords" content="symfony, project" />
 <meta name="language" content="en" />
-<title>symfony project</title>
+<title>iRank - Poker Ranking</title>
+
+<style>
+a {
+	
+	color: 				#E82314;
+	text-decoration: 	none;
+}
+
+img {
+	
+	border: 	0px;
+}
+</style>
 
 <link rel="shortcut icon" href="/favicon.ico" />
 <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $path ?>/sf/sf_default/css/screen.css" />
@@ -26,21 +37,22 @@
   <div class="sfTMessageContainer sfTAlert">
     <img alt="page not found" class="sfTMessageIcon" src="<?php echo $path ?>/sf/sf_default/images/icons/tools48.png" height="48" width="48" />
     <div class="sfTMessageWrap">
-      <h1>Oops! Ocorreu um erro</h1>
-      <h4>O servidor retornou um "500 Erro interno de servidor".</h4>
+      <h1>Ops! Ocorreu um erro</h1>
+      <h4>O servidor se comportou de forma inesperada.</h4>
     </div>
   </div>
 
   <dl class="sfTMessageInfo">
-    <dt>Alguma coisa está com problemas</dt>
-    <dd>Por favor, envie-nos um e-mail pelo <?php echo link_to('formulário de contato', '/contact') ?> e conte-nos exatamente o que aconteceu para que esse erro ocorresse. Nós iremos corrigi-lo o mais rápido possível.
+    <dt>Alguma coisa está com problema</dt>
+    <dd>Por favor, envie-nos um e-mail pelo <a href="/index.php/contact">formulário de contato</a> e conte-nos exatamente o que aconteceu para que esse erro ocorresse.<br/>
+    Iremos corrigi-lo o mais rápido possível.<br/>
     Desculpe-nos pelo inconveniente.</dd>
 
+	<br/>
   <dt>O que fazer agora?</dt>
   <dd>
     <ul class="sfTIconList">
       <li class="sfTLinkMessage"><a href="javascript:history.go(-1)">Voltar para a página anterior</a></li>
-      <li class="sfTLinkMessage"><?php echo link_to('Ir para a página principal', '@homepage') ?></li>
     </ul>
   </dd>
   </dl>
