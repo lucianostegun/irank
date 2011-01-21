ab006001bd74acedeb4b4abd91dea8b890f90304
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2011-01-21 00:59:12 +0000

    Inclusão de ferramentas de compartilhamento Modificação da página de erro 404

---
+++ trunk/apps/frontend/modules/home/templates/_include/addthis.php
@@ -0,0 +1,11 @@
+<!-- AddThis Button BEGIN -->
+<div id="shareIt">Comaprtilhe...</div>
+<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+<a class="addthis_button_preferred_1"></a>
+<a class="addthis_button_preferred_2"></a>
+<a class="addthis_button_preferred_3"></a>
+<a class="addthis_button_preferred_4"></a>
+<a class="addthis_button_compact"></a>
+</div>
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=xa-4d38d8e20e8b607f"></script>
+<!-- AddThis Button END -->
--- trunk/data/symfony/modules/default/templates/error404Success.php
+++ trunk/data/symfony/modules/default/templates/error404Success.php
@@ -1,21 +1,22 @@
-<?php decorate_with(sfLoader::getTemplatePath('default', 'defaultLayout.php')) ?>
-
+<?php sfContext::getInstance()->getResponse()->addStylesheet('/sf/sf_default/css/screen'); ?>
+<div class="commonBar"><span>Página não encontrada</span></div>
 <div class="sfTMessageContainer sfTAlert"> 
   <?php echo image_tag('/sf/sf_default/images/icons/cancel48.png', array('alt'=>'page not found', 'class'=>'sfTMessageIcon', 'size'=>'48x48')) ?>
   <div class="sfTMessageWrap">
-    <h1>Oops! Página não encontrada</h1>
+    <h1>Ops! Página não encontrada</h1>
     <h4>O servidor retornou um erro 404.</h5>
   </div>
 </div>
+<br/><br/>
 <dl class="sfTMessageInfo">
-  <dt>Você digitou a URL?</dt>
-  <dd>Você pode ter digitado o endereço (URL) incorretamente. Verifique-o para ter certeza de que você digitou corretamente a ortografia, acentuação, etc.</dd>
+  <dt>Digitou a URL certa?</dt>
+  <dd>Você pode ter digitado o endereço (URL) incorretamente. Verifique-o para ter certeza de que você digitou corretamente a ortografia, acentuação, etc.</dd><br/>
 
-  <dt>Você chegou aqui por um link de algum lugar deste site?</dt>
-  <dd>Se você encontrou esta página através de outra página deste sita, por favor envie um e-mail para nós pelo <?php echo link_to('formulário de contato', '/contact') ?> para que nos possamos corrigi-lo.</dd>
+  <dt>Chegou aqui por um link de algum lugar do site?</dt>
+  <dd>Se você encontrou esta página através de outra página deste site, por favor envie-nos um e-mail pelo <?php echo link_to('formulário de contato', '/contact') ?> para que nos possamos corrigir o problema.</dd><br/>
 
-  <dt>Você chegou aqui por um link de outro site?</dt>
-  <dd>Links de outros sites as vezes podem estar desatualizados ou incorretos. Envie-nos um e-mail para <?php echo link_to('formulário de contato', '/contact') ?> informando o site por onde você chegou aqui para que nós possamos entrar em contato para a correção do problema.</dd>
+  <dt>Chegou aqui por um link de outro site?</dt>
+  <dd>Links de outros sites podem estar desatualizados ou incorretos. Envie-nos um e-mail pelo <?php echo link_to('formulário de contato', '/contact') ?> informando o site por onde você chegou aqui para que possamos entrar em contato com eles.</dd><br/>
 
   <dt>O que fazer agora?</dt>
   <dd>
