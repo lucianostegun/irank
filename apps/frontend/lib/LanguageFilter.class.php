c4399797196d61ac13501ba4dc9a368a963d21e7
Luciano Stegun <lucianostegun@gmail.com>
2011-02-13 04:37:04 +0000

    Correção da identificação de idioma nos dispositivos móveis

--- apps/frontend/lib/LanguageFilter.class.php
+++ apps/frontend/lib/LanguageFilter.class.php
@@ -5,12 +5,15 @@ class LanguageFilter extends sfFilter {
     public function execute($filterChain) {
 
 		$userAgent = $_SERVER['HTTP_USER_AGENT'];
-		$culture   = ereg('[a-z]{2}[-_][A-Z]{2}', $userAgent, $matches);
+		$culture   = ereg('[a-z]{2}[-_][a-zA-Z]{2}', $userAgent, $matches);
 		$culture   = str_replace('-', '_', $matches[0]);
 		
+		$culture = explode('_', $culture);
+		$culture = $culture[0].'_'.strtoupper($culture[1]);
+
 		if( !ereg('^[a-z]{2}\_[A-Z]{2}$', $culture))
 			$culture = 'en_US';
-		
+			
         if( $this->isFirstCall() && !MyTools::getAttribute('culture') )
             sfContext::getInstance()->getUser()->setCulture($culture);                   
         
