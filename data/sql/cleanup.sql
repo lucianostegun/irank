232a40c2c4869a0293e00723b2f2ea73b309bd00
Luciano Stegun <lucianostegun@gmail.com>
2011-02-26 21:57:22 +0000

    Inclusão dos arquivos para a versão Sahi do site

--- data/sql/cleanup.sql
+++ data/sql/cleanup.sql
@@ -11,14 +11,17 @@ DELETE FROM home_wall;
 DELETE FROM log;
 DELETE FROM user_site;
 DELETE FROM user_site_option;
+DELETE FROM user_admin;
 DELETE FROM people;
+
 
 SELECT SETVAL('event_seq', (SELECT MAX(id) FROM event));
 SELECT SETVAL('ranking_seq', (SELECT MAX(id) FROM ranking));
 SELECT SETVAL('people_seq', (SELECT MAX(id) FROM people));
 SELECT SETVAL('user_site_seq', (SELECT MAX(id) FROM user_site));
-
+/*
 DELETE FROM virtual_table_i18n;
 DELETE FROM virtual_table;
 
 SELECT SETVAL('virtual_table_seq', (SELECT MAX(id) FROM virtual_table));
+*/
