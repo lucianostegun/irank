273b0a205c6d2e55ad4360077650f744047f2504
Luciano Stegun <lucianostegun@gmail.com>
2011-04-28 20:09:09 +0000

    Modificação da barra de compartilhamento

--- apps/frontend/modules/home/templates/_include/addthis.php
+++ apps/frontend/modules/home/templates/_include/addthis.php
@@ -1,5 +1,4 @@
 <!-- AddThis Button BEGIN -->
-<div id="shareIt"><?php echo __('leftBar.shareIt') ?>...</div>
 <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
 <a class="addthis_button_preferred_1"></a>
 <a class="addthis_button_preferred_2"></a>
@@ -7,5 +6,6 @@
 <a class="addthis_button_preferred_4"></a>
 <a class="addthis_button_compact"></a>
 </div>
-<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=xa-4d38d8e20e8b607f"></script>
+<script type="text/javascript">var addthis_config = {"data_track_clickback":true};</script>
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4db9c74a62c8d6f3"></script>
 <!-- AddThis Button END -->
