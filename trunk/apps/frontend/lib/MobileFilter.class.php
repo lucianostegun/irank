9f3116c82fbc7b8f1994f1cef106a24ac35f8e0e
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2011-05-19 01:48:45 +0000

    Correções do conflito no filtro de acesso mobile

--- trunk/apps/frontend/lib/MobileFilter.class.php
+++ trunk/apps/frontend/lib/MobileFilter.class.php
@@ -1,27 +1,34 @@
 <?php
 
 class MobileFilter extends sfFilter {
-    
+
     public function execute($filterChain) {
-        
+
         $smartPhoneList = array('iPhone', 'windows ce', 'netfront', 'palmos', 'blazer', 'elaine', 'plucker', 'avantgo', 'wap', 'android');
-        
+
         $moduleName = MyTools::getContext()->getModuleName();
         $actionName = MyTools::getContext()->getActionName();
-        
+
         $browser = $_SERVER['HTTP_USER_AGENT'];
-        
-        $forceClassic = MyTools::getAttribute('forceClassic');
-        
+
         $url = $moduleName.'/'.$actionName;
-        
+
         $quitActionList = array('event/facebookResultImage');
 
-		if( !in_array($url, $quitActionList) )
-	        foreach ($smartPhoneList as $smartPhone)            
-	            if ( !$forceClassic && stristr( $browser, $smartPhone ) )
-					return sfContext::getInstance()->getController()->redirect('/mobile.php');
-					
+        $forceClassic = MyTools::getRequestParameter('fc');
+        $forceClassic = MyTools::getAttribute('forceClassic', $forceClassic);
+
+        if( $forceClassic ){
+
+                MyTools::setAttribute('forceClassic', '1');
+        }else{
+
+                if( !in_array($url, $quitActionList) )
+                foreach ($smartPhoneList as $smartPhone)
+                    if ( !$forceClassic && stristr( $browser, $smartPhone ) )
+                        return sfContext::getInstance()->getController()->redirect('http://m.irank.com.br');
+        }
+
         $filterChain->execute();
     }
 }
