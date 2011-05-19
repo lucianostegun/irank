a494a8291188635e6e7cb811cb773d03f5a94afe
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2011-05-19 01:55:03 +0000

    Modifcações para visualizar o resultado do evento pela versão mobile

--- trunk/apps/frontend/lib/MobileFilter.class.php
+++ trunk/apps/frontend/lib/MobileFilter.class.php
@@ -2,36 +2,36 @@
 
 class MobileFilter extends sfFilter {
 
-    public function execute($filterChain) {
+	public function execute($filterChain) {
 
-        $smartPhoneList = array('iPhone', 'windows ce', 'netfront', 'palmos', 'blazer', 'elaine', 'plucker', 'avantgo', 'wap', 'android');
+		$smartPhoneList = array('iPhone', 'windows ce', 'netfront', 'palmos', 'blazer', 'elaine', 'plucker', 'avantgo', 'wap', 'android');
 
-        $moduleName = MyTools::getContext()->getModuleName();
-        $actionName = MyTools::getContext()->getActionName();
+		$moduleName = MyTools::getContext()->getModuleName();
+		$actionName = MyTools::getContext()->getActionName();
 
-        $browser = $_SERVER['HTTP_USER_AGENT'];
+		$browser = $_SERVER['HTTP_USER_AGENT'];
 
-        $url = $moduleName.'/'.$actionName;
+		$url = $moduleName.'/'.$actionName;
 
-        $quitActionList = array('event/facebookResultImage');
+		$quitActionList = array('event/facebookResult', 'event/facebookResultImage');
 
-        $forceClassic = MyTools::getRequestParameter('fc');
-        $forceClassic = MyTools::getAttribute('forceClassic', $forceClassic);
+		$forceClassic = MyTools::getRequestParameter('fc');
+		$forceClassic = MyTools::getAttribute('forceClassic', $forceClassic);
 
-        if( $forceClassic ){
+		if( $forceClassic ){
 
-                MyTools::setAttribute('forceClassic', '1');
-        }else{
+				MyTools::setAttribute('forceClassic', '1');
+		}else{
 
 			if( !in_array($url, $quitActionList) ){
-                
-                foreach ($smartPhoneList as $smartPhone)
-                    if ( !$forceClassic && stristr( $browser, $smartPhone ) )
-                        return sfContext::getInstance()->getController()->redirect('http://m.irank.com.br');
+				
+				foreach ($smartPhoneList as $smartPhone)
+					if ( !$forceClassic && stristr( $browser, $smartPhone ) )
+						return sfContext::getInstance()->getController()->redirect('http://m.irank.com.br');
 			}
-        }
+		}
 
-        $filterChain->execute();
-    }
+		$filterChain->execute();
+	}
 }
 ?>
