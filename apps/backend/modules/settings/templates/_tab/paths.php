7878650779fb4e16eeaa8835821ffc934ec9e5c3
Luciano Stegun <lucianostegun@gmail.com>
2012-04-14 23:32:30 +0000

    Remocao da pasta settings

--- apps/backend/modules/settings/templates/_tab/paths.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$htpasswdFilePath = Config::getConfigByName('htpasswdFilePath', true);
-?>
-	<div class="formRow">
-		<label>Arquivo .htpasswd</label>
-		<div class="formRight"><?php echo input_tag('htpasswdFilePath', $htpasswdFilePath, array('size'=>50, 'id'=>'settingsHtpasswdFilePath')) ?></div>
-		<div class="clear"></div>
-	</div>
