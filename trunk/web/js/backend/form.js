8ad8ff431995f983a026700712ac96cdf2e76fb6
guilhermelimpo <guilhermelimpo@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2012-04-18 23:41:49 +0000

    Validaçao dos campos

--- trunk/web/js/backend/form.js
+++ trunk/web/js/backend/form.js
@@ -2,14 +2,43 @@ function updateMainRecordName(){
 	
 }
 
-function handleFormFieldError(){
+function handleFormFieldError( content, instanceName ){
 	
-	debugAdd('handleFormFieldError')
+	if( (/^formError:/).test(content) ){
+
+       clearFormFieldErrors(instanceName);
+
+       content = content.replace(/^formError:/, '');
+
+       var formErrorObj    = parseInfo(content);
+       var fieldNameList   = formErrorObj._fieldNameList
+       var fieldErrorCount = formErrorObj._fieldErrorCount
+
+       for(var i=0; i < fieldErrorCount; i++){
+
+           var fieldName    = fieldNameList[i];
+           var errorMessage = formErrorObj[fieldName];
+           
+           if( $('#'+instanceName+ucfirst(fieldName))!=null ){
+        	  
+               $('#'+instanceName+ucfirst(fieldName)).attr('title', errorMessage);
+               $('#'+instanceName+ucfirst(fieldName)).attr('class', 'fieldError');
+           }
+       }
+	}else{
+		
+       alert('Não foi possível concluir seu cadastro!\nPor favor, tente novamente.');
+	}
 }
 
-function clearFormFieldErrors( formId ){
+function clearFormFieldErrors(formId){
 
-	hideFormStatusError()
+   var formObj = document.getElementById(formId+'Form');
+   
+   var errorList = formObj.getElementsByClassName('fieldError');
+
+   for(var i=0; i < errorList.length; i++)
+	   errorList[i].innerHTML = '';
 }
 
 function hideFormStatusSuccess(){
