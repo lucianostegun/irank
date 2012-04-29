2724ca7015216f0c21ee3bc18f4f94d598cc0f31
guilhermelimpo <guilhermelimpo@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2012-04-29 21:45:07 +0000

    Publicar no twitter incompleto

--- trunk/apps/backend/modules/eventLive/templates/_tab/disclosure.php
+++ trunk/apps/backend/modules/eventLive/templates/_tab/disclosure.php
@@ -1,7 +1,14 @@
 	<div class="formRow">
 		<br/>
+		
 		<?php echo link_to( 'Divulgar por Email', '#eventLiveEmailShare()') ?><br/><br/>
+		
 		<?php echo link_to( 'Divulgar no Facebook', '#eventLiveFacebookShare()') ?><br/><br/>
-		<?php echo link_to( 'Divulgar no Twitter', '#eventLiveTwitterShare()') ?><br/><br/>
+		
+		<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://irank/index.php/eventLive/details/eventLiveId/<?php echo $eventLiveId ?>" data-lang="pt" data-count="none" target="_blank">Divulgar no Twitter</a>
+		<br/><br/>
+		
 		<div class="clear"></div>
 	</div>
+	
+	
