79fa3710c9c325a3b857df388a9c9cdf8bb19e89
Luciano Stegun <lucianostegun@gmail.com>
2012-05-05 19:49:55 +0000

    Ajuste na visualização de detalhes de clubes

--- apps/frontend/modules/club/templates/detailsSuccess.php
+++ apps/frontend/modules/club/templates/detailsSuccess.php
@@ -11,6 +11,7 @@
 	$clubSite        = $clubObj->getClubSite();
 	$clubSiteLink    = ($clubSite?(preg_match('/^[a-zA-Z0-9\-_\.]*@/', $clubSite)?'mailto:'.$clubSite:'http://'.$clubSite):null);
 	$clubSiteTarget  = ($clubSite?(preg_match('/^[a-zA-Z0-9\-_\.]*@/', $clubSite)?'_top':'_blank'):null);
+	$clubSite        = str_ireplace('http://', '', $clubSite);
 	
 	$pathList = array('Onde jogar'=>$moduleName.'/index', 
 					  $clubName=>null);
@@ -20,16 +21,15 @@
 <div class="clubDetailsArea" align="center">
 	<table cellspacing="0" cellpadding="0" width="99%" class="clubDetails">
 		<tr>
-			<td class="logo"><?php echo image_tag('club/'.$clubObj->getFileNameLogo()) ?></td>
-			<td valign="top">
-				<table cellspacing="0" cellpadding="0" class="clubDetails">
+			<td class="logo" style="width: 150px"><?php echo image_tag('club/'.$clubObj->getFileNameLogo()) ?><br/><?php echo image_tag('blank.gif', array('style'=>'width: 130px; height: 1px;')) ?></td>
+			<td valign="top"><table cellspacing="0" cellpadding="0" class="clubDetails">
 					<tr>
 						<td valign="top" class="info" colspan="2">
 							<h1><?php echo $clubName ?></h1>
 						</td>
 					</tr>
 					<tr>
-						<td valign="top" class="info" rowspan="5">
+						<td valign="top" class="info" rowspan="2">
 							<div class="location">
 								<?php echo $addressName.', '.$addressNumber.($addressQuarter?', '.$addressQuarter:'') ?><br/>
 								<?php echo $city ?>-<?php echo $state ?>
@@ -43,13 +43,13 @@
 					</tr>
 					<?php endif; ?>
 					<tr>
-						<th class="phones"><div class="separator"></div>Telefones:</th>
+						<th class="phones" colspan="3">
+							<div class="separator"></div><label>Telefones:</label>
+							<?php foreach($clubObj->getPhoneNumberList() as $phoneNumber): ?>
+							<div class="phone"><?php echo $phoneNumber ?></div>
+							<?php endforeach; ?>
+						</th>
 					</tr>
-					<?php foreach($clubObj->getPhoneNumberList() as $phoneNumber): ?>
-					<tr>
-						<td class="phone"><?php echo $phoneNumber ?></td>
-					</tr>
-					<?php endforeach; ?>
 				</table>
 			</td>
 		</tr>
