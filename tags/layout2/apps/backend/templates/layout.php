6e474f53ad8a56481c61012beb648d023705d082
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2012-06-04 21:58:28 +0000

    Ajustes no layout

--- tags/layout2/apps/backend/templates/layout.php
+++ tags/layout2/apps/backend/templates/layout.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt" lang="pt">
 <head>
 
 <?php
