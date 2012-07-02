d33c7acfb7afc5f1342336e356a86cb737ba0b57
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2012-07-02 02:37:53 +0000

    Progresso no desenvolvimento da loja virtual

--- trunk/apps/frontend/modules/store/templates/_include/cart.php
+++ trunk/apps/frontend/modules/store/templates/_include/cart.php
@@ -2,7 +2,7 @@
 	<div class="cartIcon"><?php echo image_tag('store/cart') ?></div>
 	<?php echo button_tag('storeCart', 'Meu carrinho', array('onclick'=>'goToPage("store", "cart")', 'style'=>'margin-left: 35px')) ?>
 	<div class="cartInfo">
-	<?php echo link_to('Você possui <b>3</b> itens em seu <u><b>carrinho</b></u>.<br/>Total atual: <b>R$ 119,70</b>', 'store/cart') ?><br/>
+	Você possui <b>3</b> itens em seu <u><b><?php echo link_to('carrinho', 'store/cart') ?></b></u>.<br/>Total atual: <b>R$ 119,70</b><br/>
 	</div>
 	<div class="clear"></div>
 </div>
