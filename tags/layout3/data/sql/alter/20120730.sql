a6f864006ae233540a70e4fbad44daf3a4bd3eea
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2012-08-02 03:48:05 +0000

    Ajustes na divulgação de resultados de eventos no Facebook

--- tags/layout3/data/sql/alter/20120730.sql
+++ tags/layout3/data/sql/alter/20120730.sql
@@ -9,21 +9,3 @@ CREATE TABLE product_item_stock_log (
     comments VARCHAR(200),
     created_at TIMESTAMP
 );
-
-SELECT
-    stock+
-    (SELECT SUM(stock) FROM product_item_stock_log WHERE product_item_id=product_item.ID)-
-    COALESCE((SELECT 
-        SUM(quantity) 
-     FROM 
-        purchase_product_item
-        INNER JOIN purchase ON purchase_product_item.PURCHASE_ID=purchase.ID
-     WHERE 
-        purchase_product_item.PRODUCT_ITEM_ID=product_item.ID
-        AND purchase.ORDER_STATUS IN ('shipped', 'complete')), 0)
-FROM 
-    product_item 
-WHERE 
-    visible 
-    AND enabled 
-    AND NOT deleted
--- tags/layout3/data/sql/alter/20120730_log.sql
+++ tags/layout3/data/sql/alter/20120730_log.sql
@@ -1,4 +1,2 @@
 ALTER TABLE log DROP COLUMN updated_at;
 ALTER TABLE log_field DROP COLUMN updated_at;
-ALTER TABLE purchase_transaction_log DROP COLUMN updated_at;
-ALTER TABLE purchase_status_log DROP COLUMN updated_at;
