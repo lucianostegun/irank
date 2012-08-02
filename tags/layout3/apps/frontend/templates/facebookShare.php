d15efbedd6e7e339349f0000b8fde06ea20d0a54
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2012-08-02 03:53:10 +0000

    Ajustes na divulgação de resultados de eventos no Facebook

--- tags/layout3/apps/frontend/templates/facebookShare.php
+++ tags/layout3/apps/frontend/templates/facebookShare.php
@@ -1,19 +1,26 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-BR" lang="pt-BR">
 <head>
-<meta property="og:title" content="<?php echo $metaTitle ?>" />
-<meta property="og:type" content="game" />
-<meta property="og:description" content="<?php echo $metaDescription ?>" />
-<meta property="og:url" content="http://<?php echo $sf_request->getHost() ?>/<?php echo $shareLink ?>" />
+<link rel="icon" href="http://www.irank.com.br/favicon.ico" type="image/x-icon"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="title" content="<?php echo $metaTitle ?>" />
+<meta name="description" content="<?php echo $metaDescription ?>" />
+<meta name="language" content="pt" />
 <meta property="og:image" content="<?php echo $metaImage ?>" />
-<meta property="og:image" content="http://<?php echo $sf_request->getHost() ?>/images/layout/mediaShare.png" />
+<meta property="og:url" content="http://www.irank.com.br" />
 <meta property="og:site_name" content="iRank - Poker Ranking" />
-<meta property="fb:admins" content="1424201846" />
-</head>
+<meta property="og:image" content="http://<?php echo $sf_request->getHost() ?>/images/layout/mediaShare.png" />
+
 <body>
 <script type="text/javascript">
 function init() { window.location = 'http://<?php echo $sf_request->getHost() ?>/<?php echo $shareLink ?>'; }
-window.onload = init;
+//window.onload = init;
 </script>
+
+</head>
+<body>
+
+
 </body>
 </html>
 <?php exit; ?>
