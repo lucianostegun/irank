102c9f7c12fab11781192700cdba276aa046cffd
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2012-08-22 05:16:24 +0000

    Modificações na listagem de artigos no blog

--- tags/layout3/apps/frontend/modules/blog/templates/_include/suggest.php
+++ tags/layout3/apps/frontend/modules/blog/templates/_include/suggest.php
@@ -14,8 +14,10 @@
 	
 	foreach($blogObjList as $blogObj):
 ?>
+	<a href="<?php echo url_for('blog/article?'.$blogObj->getPermalink().'=') ?>">
 	<h1><?php echo $blogObj->getCreatedAt('d/m/Y H\hm') ?></h1><h2><?php echo $blogObj->getTitle() ?></h2>
 	<h3><?php echo $blogObj->getCaption() ?></h3>
 	<div class="clear"></div>
+	</a>
 <?php endforeach; ?>
 </div>
