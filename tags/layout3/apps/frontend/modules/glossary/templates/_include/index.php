efed1942ef3dc73f30bdeebb5cba733cb4569050
lucianostegun <lucianostegun@edee616e-cf2f-41aa-ac4a-9f7e4d52ea2b>
2013-01-23 15:13:00 +0000

    Sincronização das modificações feitas no ambiente de produção

--- tags/layout3/apps/frontend/modules/glossary/templates/_include/index.php
+++ tags/layout3/apps/frontend/modules/glossary/templates/_include/index.php
@@ -1,13 +1,19 @@
-<div class="glossaryIndex">
+<div class="glossaryTermFilter">
 <?php
 	if( $letter )
 		$letterList = array($letter);
 	else
-		$letterList = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','#');
+		$letterList = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','#');
 	
+	foreach($letterList as $letter)
+		echo link_to($letter, '##'.($letter=='#'?'number':$letter));
+?>
+</div>
+<div class="glossaryIndex">
+<?php
 	foreach($letterList as $letter):
 ?>
-<h1 class="glossaryLetter"><?php echo $letter ?></h1>
+<h1 class="glossaryLetter" id="<?php echo ($letter=='#'?'number':$letter) ?>"><?php echo $letter ?></h1>
 <hr/>
 <?php
 	$criteria = new Criteria();
